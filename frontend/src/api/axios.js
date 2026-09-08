78364f61d7cc5d1909417abe8dfd5e859f10d225
Muhammad-Zohaib-Malik <muhammadzohaibmalik10@gmail.com>
2026-09-08 11:37:42 +0500

    add backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://super-store-erp.onrender.com/api/v1",
   withCredentials: true, // Required to send the HTTP-only access token cookie
 });
 
